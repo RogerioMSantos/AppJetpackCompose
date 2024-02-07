d9aa26e3215cc608e6743f62c0822d90edc35722
rogerioMSantos <rogerio.rmsj@gmail.com>
2024-02-07 10:43:51 -0300

    fix: Calculadora funcionando

--- app/src/main/java/com/example/app/CalculadoraService.kt
+++ app/src/main/java/com/example/app/CalculadoraService.kt
@@ -19,8 +19,10 @@ class CalculadoraService : Service(){
     fun calculaResultados(visor: String): String{
         val digitos = digitos(visor)
         if(digitos.isEmpty()) return ""
-        val result = calculaResultadosRecursivo(digitos)
-        return result.last().toString()
+        val result = calculaResultadosRecursivo(digitos).last() as Float
+        if(result % 1 == 0f)
+            return result.toInt().toString()
+        return result.toString()
     }
 
     fun calculaResultadosRecursivo(digitos: MutableList<Any>): MutableList<Any>{
@@ -97,7 +99,17 @@ class CalculadoraService : Service(){
     }
 
     private fun multDivCalculo(digitos: MutableList<Any>): MutableList<Any> {
-        var list = digitos
+        var qtdParenteses = 0
+        var limite = digitos.size
+        for(i in digitos.indices){
+            if(digitos[i] == '(')
+                qtdParenteses ++
+            if(digitos[i] == ')'){
+                if (qtdParenteses == 0) limite = i
+                else qtdParenteses--
+            }
+        }
+        var list = digitos.subList(0,limite)
         while (list.contains('x') || list.contains('/') || list.contains('%'))
             list = calcMultDiv(list)
         return list
@@ -105,9 +117,16 @@ class CalculadoraService : Service(){
 
     private fun calcMultDiv(list: MutableList<Any>): MutableList<Any> {
         val newList = mutableListOf<Any>()
-        var restartIndex = list.size
         var qtdParenteses = 0
         var parenteses = false
+        var restartIndex = list.size
+        if(list[0] == '('){
+            parenteses = true
+            newList.add(calculaResultadosRecursivo(list.subList(1,list.lastIndex)).last())
+        }
+        else if(list[0] == ')'){
+            return newList
+        }
         for(i in list.indices) {
             if(parenteses) {
                 if (list[i] == '(') {
@@ -120,46 +139,50 @@ class CalculadoraService : Service(){
                     continue
                 }
             }
-            if (list[i] is Char && i != list.lastIndex && i < restartIndex) {
-                val operador = list[i]
-                if (operador == ')') return newList
-                val digitoAnt = list[i - 1] as Float
-                var digitoProx = list[i + 1]
-                if (digitoProx == ')') return newList
-                if (digitoProx == '(') {
-                    digitoProx = calculaResultadosRecursivo(
-                        list.subList(
-                            i + 2,
-                            list.lastIndex + 1
-                        )
-                    ).last() as Float
-                    parenteses = true
-                } else digitoProx = digitoProx as Float
-                when (operador) {
-                    'x' -> {
-                        newList.add(digitoAnt * digitoProx)
-                        restartIndex = i + 1
+            else {
+                if (list[i] is Char && i != list.lastIndex && i < restartIndex) {
+                    val operador = list[i]
+                    if (operador == ')') return newList
+                    var digitoAnt = 0f
+                    if(list[i-1] == ')') digitoAnt = newList.removeLast() as Float
+                    else digitoAnt = list[i - 1] as Float
+                    var digitoProx = list[i + 1]
+                    if (digitoProx == ')') return newList
+                    if (digitoProx == '(') {
+                        digitoProx = calculaResultadosRecursivo(
+                            list.subList(
+                                i + 2,
+                                list.lastIndex + 1
+                            )
+                        ).last() as Float
+                        parenteses = true
+                    } else digitoProx = digitoProx as Float
+                    when (operador) {
+                        'x' -> {
+                            newList.add(digitoAnt * digitoProx)
+                            restartIndex = i + 1
+                        }
+
+                        '/' -> {
+                            newList.add(digitoAnt / digitoProx)
+                            restartIndex = i + 1
+                        }
+
+                        '%' -> {
+                            newList.add(digitoAnt * digitoProx / 100)
+                            restartIndex = i + 1
+                        }
+
+                        else -> {
+                            newList.add(digitoAnt)
+                            newList.add(operador)
+                        }
                     }
 
-                    '/' -> {
-                        newList.add(digitoAnt / digitoProx)
-                        restartIndex = i + 1
-                    }
-
-                    '%' -> {
-                        newList.add(digitoAnt * digitoProx / 100)
-                        restartIndex = i + 1
-                    }
-
-                    else -> {
-                        newList.add(digitoAnt)
-                        newList.add(operador)
-                    }
                 }
-
+                if (i > restartIndex)
+                    newList.add(list[i])
             }
-            if (i > restartIndex)
-                newList.add(list[i])
         }
         return newList
     }
@@ -171,20 +194,21 @@ class CalculadoraService : Service(){
 
         for(caracter in visor){
             if(caracter == '(' && digitoAtual.isNotBlank()){
-                lista.add(digitoAtual)
+                lista.add(digitoAtual.toFloat())
                 digitoAtual = ""
-                lista.add('*')
+                lista.add('x')
                 lista.add('(')
                 continue
             }
 
             if(caracter == ')' && digitoAtual.isNotBlank()){
-                lista.add(digitoAtual)
+                lista.add(digitoAtual.toFloat())
                 digitoAtual = ""
                 lista.add(')')
                 continue
             }
             if(caracter.isDigit() || caracter ==',' || caracter == '.'){
+                if(lista.size > 0 && lista.last() == ')') lista.add('x')
                 if (caracter == ',') digitoAtual +='.'
                 else digitoAtual += caracter
             }
